87214d47eed730e150f81fa9bdf2268aa9bc9274
pbrosamond <pbrosamond@gmail.com>
2024-02-02 16:24:44 -0800

    Cleaned up code. Downloaded a new library.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const videoRoutes = require('./routes/videos');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 // const { VERSION, BASE_PATH } = process.env; //It's one or the other not both.
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(`/videos`, videoRoutes);
 
--- routes/videos.js
+++ routes/videos.js
@@ -1,61 +1,92 @@
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const fetch = require("node-fetch");
+const path = require("path");
 
 const router = express.Router();
 
 // Load initial data from the JSON file
 let videosData = loadVideosData();
 
-router.route('/')
+router
+  .route("/")
   .get((req, res) => {
-    res.json(videosData);
+    const newVideoData = JSON.parse(JSON.stringify(videosData)).map(el => {
+      if (!el.image.match(/^https?:\/\//)) { 
+        el.image = `http://127.0.0.1:5050/upload-images/${el.image}`
+      }
+      delete el.views;
+      delete el.likes;
+      delete el.duration;
+      delete el.video;
+      delete el.timestamp;
+      delete el.comments;
+      return el;
+    }); 
+    res.json(newVideoData);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     const newVideo = req.body;
-    if (!newVideo.title || !newVideo.description) return res.status(404).json("All new video uploads must have a title and a description") 
+    if (!newVideo.title || !newVideo.description || !newVideo.image)
+      return res
+        .status(404)
+        .send("All new video uploads must have a title and a description");
+
     newVideo.id = uuidv4(); // Use uuid to generate a unique ID
+
+    const imageFetchResponse = await fetch(newVideo.image);
+    const extName = path.extname(newVideo.image);
+    const fileName = newVideo.id + extName
+
+    imageFetchResponse.body.pipe(fs.createWriteStream(`./public/upload-images/${fileName}`));
+
+    newVideo.image = fileName
     newVideo.channel = "Lord of the Abyss";
-    newVideo.image = "https://pbs.twimg.com/media/FPLgzZNXoAoy_1T?format=jpg&name=large";
     newVideo.views = "Too many to count";
     newVideo.likes = "Too many to count";
     newVideo.duration = "The perfect duration";
-    newVideo.video = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; 
-    newVideo.timestamp = new Date().getTime(); 
+    newVideo.video = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    newVideo.timestamp = new Date().getTime();
     newVideo.comments = [];
     videosData.push(newVideo);
-  
+
     // Update the JSON file with the new data
     saveVideosData(videosData);
-  
+
     res.status(201).json(newVideo);
-})
+  });
 
 // Endpoint to get a specific video by ID
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const videoId = req.params.id;
   const video = videosData.find((video) => video.id === videoId);
 
+  const newVideo = JSON.parse(JSON.stringify(video))
+  if (!newVideo.image.match(/^https?:\/\//)) { 
+  newVideo.image = `http://127.0.0.1:5050/upload-images/${newVideo.image}`
+  }
+
   if (video) {
-    res.status(200).json(video);
+    res.status(200).json(newVideo);
   } else {
-    res.status(404).json({ error: 'Video not found' });
+    res.status(404).json({ error: "Video not found" });
   }
 });
 
 // Helper function to load video data from the JSON file
 function loadVideosData() {
   try {
-    const data = fs.readFileSync('./data/videos.json', 'utf8');
+    const data = fs.readFileSync("./data/videos.json", "utf8");
     return JSON.parse(data);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
 // Helper function to save video data to the JSON file
 function saveVideosData(data) {
-  fs.writeFileSync('./data/videos.json', JSON.stringify(data, null, 2), 'utf8');
+  fs.writeFileSync("./data/videos.json", JSON.stringify(data, null, 2), "utf8");
 }
 
 module.exports = router;
